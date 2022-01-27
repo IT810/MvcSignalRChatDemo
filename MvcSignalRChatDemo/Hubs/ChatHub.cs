b7aa8e3b06fbbe659410765556845f9b228a7923
Hung Nguyen <hung.187pm20486@vanlanguni.vn>
2022-01-27 18:56:34 +0700

    Add project files.

---
+++ MvcSignalRChatDemo/Hubs/ChatHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MvcSignalRChatDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        static ConcurrentDictionary<string, string> dic = new ConcurrentDictionary<string, string>();
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void SendToSpecific(string name, string message, string to)
+        {
+            Clients.Caller.broadcastMessage(name, message);
+            Clients.Client(dic[to]).broadcastMessage(name, message);
+        }
+
+        public void Notify(string name, string id)
+        {
+            if (dic.ContainsKey(name))
+            {
+                Clients.Caller.differentName();
+            }
+            else
+            {
+                dic.TryAdd(name, id);
+                foreach (KeyValuePair<String, String> entry in dic)
+                {
+                    Clients.Caller.online(entry.Key);
+                }
+                Clients.Others.enters(name);
+            }
+        }
+    }
+}
---
+++ MvcSignalRChatDemo/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(MvcSignalRChatDemo.Startup))]
+
+namespace MvcSignalRChatDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
